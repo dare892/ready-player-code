1cbb8c021e7d9cf6b70dbd637b102d31b72c5ab0
jong <jong.park@nikolamotor.com>
2019-09-10 08:09:01 -0700

    update

--- db/migrate/20190904185821_create_challenge_answers.rb
+++ db/migrate/20190904185821_create_challenge_answers.rb
@@ -4,6 +4,7 @@ class CreateChallengeAnswers < ActiveRecord::Migration[5.1]
       t.references :challenge
       t.text :input
       t.text :output
+      t.boolean :is_test, default: false
 
       t.timestamps
     end
