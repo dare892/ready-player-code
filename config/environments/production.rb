5550a3160e4f44de0dff5fcd98c4aebba2de4ebb
Ubuntu <ubuntu@ip-172-31-44-248.us-east-2.compute.internal>
2019-09-26 13:46:19 +0000

    cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.url = 'ws://readyplayercode.com:6379/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://readyplayercode.com', 'http://13.58.89.187', 'https://readyplayercode.com', /https:\/\/readyplayercode.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
